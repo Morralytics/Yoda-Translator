4136a358a409caf468128303c6190c424c54e4a1
unknown <peterataras@gmail.com>
2022-12-01 19:34:29 -0800

    API 1 value

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,27 @@
-var quote = "https://api.goprogram.ai/inspiration";
-fetch(quote)
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    "X-RapidAPI-Key": "2ffa236209msh40cdcf1eec77dd3p199fb7jsncccb2f0b5456",
+    "X-RapidAPI-Host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+  },
+};
+
+fetch(
+  "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random",
+  options
+)
   .then(function (response) {
     if (response.ok) {
       console.log(response);
       response.json().then(function (data) {
         console.log(data);
+        console.log(data.value);
       });
     } else {
-      alert("Error: " + response.status);
+      alert("Error: " + response.statusText);
     }
   })
   .catch(function (error) {
     alert("Unable to connect to API");
   });
-
-//   const api_url ="https://zenquotes.io/api/quotes/";
-
-// async function getapi(url, data = {
-//     method: "get",
-//     mode: "no-cors"
-//   })
-// {
-//   const response = await fetch(url);
-//   var data = await response.json();
-//   console.log(data);
-// }
-
-// getapi(api_url);

f80453b47a213710d55d154056a89970be619932
unknown <peterataras@gmail.com>
2022-12-01 20:37:47 -0800

    click function

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,47 @@
+
+var DropDowntrigger = document.querySelector('#button')
+
+
+
+
+
+// var yodaTranslate = function (input) {
+//   var yoda =
+//     "https://api.funtranslations.com/translate/yoda.json?text=" + input + ".";
+//   fetch(yoda)
+//     .then(function (response) {
+//       if (response.ok) {
+//         console.log(response);
+//         response.json().then(function (data) {
+//           console.log(data);
+//         });
+//       } else {
+//         alert("Error: " + response.statusText);
+//       }
+//     })
+//     .catch(function (error) {
+//       alert("Unable to connect to API");
+//     });
+// };
+
+
 const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    "X-RapidAPI-Key": "2ffa236209msh40cdcf1eec77dd3p199fb7jsncccb2f0b5456",
-    "X-RapidAPI-Host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
-  },
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2ffa236209msh40cdcf1eec77dd3p199fb7jsncccb2f0b5456',
+		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+	}
 };
 
-fetch(
-  "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random",
-  options
-)
-  .then(function (response) {
-    if (response.ok) {
-      console.log(response);
-      response.json().then(function (data) {
-        console.log(data);
-        console.log(data.value);
-        var input = data.value;
-        yodaTranslate(input);
-      });
-    } else {
-      alert("Error: " + response.statusText);
-    }
-  })
-  .catch(function (error) {
-    alert("Unable to connect to API");
-  });
-var yodaTranslate = function (input) {
-  var yoda =
-    "https://api.funtranslations.com/translate/yoda.json?text=" + input + ".";
-  fetch(yoda)
+fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
     .then(function (response) {
       if (response.ok) {
         console.log(response);
         response.json().then(function (data) {
           console.log(data);
+          console.log(data.content);
+          var input = data.content;
+        //   yodaTranslate(input);
         });
       } else {
         alert("Error: " + response.statusText);
@@ -44,4 +50,10 @@ var yodaTranslate = function (input) {
     .catch(function (error) {
       alert("Unable to connect to API");
     });
+
+var DropFunction = function(event){
+event.preventDefault();
+console.log('click');
 };
+
+ DropDowntrigger.addEventListener("click", DropFunction);

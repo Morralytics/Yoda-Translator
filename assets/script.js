b9dcdad3885f0dc1691955111accc9b206d3d87a
unknown <peterataras@gmail.com>
2022-12-05 21:28:38 -0800

    filter functions

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,51 @@
 var numOfFilter = 3;
 var DropDowntrigger = document.querySelector(".button");
 var dropDownShow = document.querySelector(".dropdown");
-
+var Sad = [
+  "happiness",
+  "cry",
+  "loss",
+  "death",
+  "likeness",
+  "life",
+  "doing",
+  "people",
+  "thing",
+];
+var life = ["living", "doinig", "use", "thing", "people", "likeness", "making"];
+var friendship = [
+  "family",
+  "love",
+  "life",
+  "hostility",
+  "other",
+  "friend",
+  "people",
+  "likeness",
+  "use",
+];
+var love = [
+  "Valentine's day",
+  "love",
+  "making love",
+  "partnership",
+  "gauge",
+  "romantic",
+  "cute",
+  "for her",
+  "for him",
+];
+var funny = [
+  "cute",
+  "life",
+  "people",
+  "doing",
+  "man",
+  "life",
+  "likeness",
+  "want",
+  "thing",
+];
 var yodaTranslate = function (input) {
   var yoda =
     "https://api.funtranslations.com/translate/yoda.json?text=" + input + ".";
@@ -21,10 +65,17 @@ var yodaTranslate = function (input) {
     });
 };
 
-function filterbychr(input) {
+function filterbychr(input, author) {
+    checker = /^T/;
+    checker.test(author);
+    result = (checker.test(author));
+console.log(result);
+    if (result === false){
+setTimeout(quote, 1000);
+    }else{
   input = input.replace(/[^\w\s.&-]+/g, "");
   console.log(input);
-}
+}}
 
 const options = {
   method: "GET",
@@ -33,27 +84,28 @@ const options = {
     "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
   },
 };
-
-fetch("https://quotes15.p.rapidapi.com/quotes/random/", options)
-  .then(function (response) {
-    if (response.ok) {
-      console.log(response);
-      response.json().then(function (data) {
-        console.log(data);
-        var tags = data.tags;
-        var author = data.originator.name
-        var input = data.content;
-        console.log(tags);
-        console.log(author);
-        filterbychr(input);
-      });
-    } else {
-      alert("Error: " + response.statusText);
-    }
-  })
-  .catch(function (error) {
-    alert("Unable to connect to API");
-  });
+var quote = function () {
+  fetch("https://quotes15.p.rapidapi.com/quotes/random/", options)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          var tags = data.tags;
+          var author = data.originator.name;
+          var input = data.content;
+          console.log(tags);
+          console.log(author);
+          filterbychr(input, author);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to API");
+    });
+};
 
 // Want to dynamically add filter boxes based on how many filters we decide to use
 // Dowm the road wishlist item
@@ -65,5 +117,6 @@ var DropFunction = function (event) {
   dropDownShow.classList.add("is-active");
   console.log("click");
 };
+quote();
 
 DropDowntrigger.addEventListener("click", DropFunction);

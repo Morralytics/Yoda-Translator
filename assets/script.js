0449b705e5c53e1e363fab204ca8867cd9b86c9d
Serenity <sarah.k.hopper@gmail.com>
2022-12-17 16:19:15 -0800

    update code, local storage working

--- assets/script.js
+++ assets/script.js
@@ -120,7 +120,7 @@ function getCommon(tagsL, chosenTag, input) {
 
 var yodaTranslate = function (input) {
   var yoda =
-    "https://api.funtranslations.com/translate/yoda.json?text=" + input + ".";
+    "https://api.funtranslations.com/translate/yoda.json?text=" + input;
   fetch(yoda)
     .then(function (response) {
       if (response.ok) {
@@ -137,7 +137,7 @@ var yodaTranslate = function (input) {
       }
     })
     .catch(function (error) {
-      alert("Unable to connect to API");
+      alert("Unable to connect to Yoda Translator API");
     });
 };
 
@@ -172,35 +172,6 @@ function filterbychr(input, chosenTag, tags) {
   getCommon(tagsL, chosenTag, input);
 }
 
-//need to clear storage at some point
-// function clearStorage() {
-//   localStorage.clear();
-// }
-
-//testing variables outside function (to avoid using up limited API calls)
-
-// var quoteContainer = document.querySelector("#pg2-quote-container");
-// var quotePara = document.createElement("p");
-// quotePara.setAttribute("id", "pg2-quote");
-// var testQuote = localStorage.getItem("wisdom");
-
-// window.onload = function sarahTest() {
-  // if ( (document.location.href = "/result.html") && (testQuote != null) ) { 
-    //only run fxn if on result.html and testQuote has content
-
-//   if (testQuote != null) {
-//     quotePara.textContent = '"' + testQuote + '"';
-//     quoteContainer.append(quotePara);
-//         console.log("test quote: " + testQuote)
-//         console.log("quoteContainer: " + quoteContainer)
-//         console.log("quoteBox: " + quotePara)
-//   } else {
-//     quotePara.textContent = "Yoda doesn't seem to have anything to say right now. Click the Back button to try again.";
-//     quoteContainer.append(quotePara);
-//     console.log("test quote is null.");
-//   }
-// }
-
 //fxn shows translated quote on result.html
 window.onload = function pg2Quote(YodaQuote) {
   var mostRecent = localStorage.getItem("wisdom"); //YodaQuote will save each new translated quote to "wisdom" key but if/when we start saving multiple quotes to local storage, we'll need to figure out how to grab only the most recent quote for showing on the second page
@@ -209,30 +180,7 @@ window.onload = function pg2Quote(YodaQuote) {
   quotePara.setAttribute("id", "pg2-quote"); //set <p> id to #pg2-quote for styling purposes
   quotePara.textContent = '"' + mostRecent + '"';
   quoteContainer.append(quotePara);
-
-  //append random yoda pic to page
-  // var imgEl = document.getElementById("yoda-pic");
-  // var yodaPix = newArray("/assets/images/baby-yoda-pissed.jpg", "/assets/images/yoda-1.jpg", "/assets/images/yoda-2.jpg");
-  // var randomNum = Math.floor(Math.random() * yodaPix.length);
-  // imgEl.src = yodaPix[randomNum];
 }
-
-// var yodaPic = document.getElementById("yoda-pic");
-// var yodaPix = ("./assets/images/yoda-1.jpg");
-// yodaPic.src = "./assets/images/yoda-2.jpg";
-
-  //code below is probably not necessary since API is consistently working, so there should always be a quote to show on result.html; however, we may still want to implement something like this in case something goes wrong -- without it, the page will probably display "null" as it is now
-
-    //only show text content if quote has been translated, else show 'try again' message
-
-    //     if (mostRecent != null) {
-    //       quotePara.textContent = '"' + mostRecent + '"'; //set <p> text content to most recently translated quote
-    //       quoteContainer.append(quotePara); //append <p> to div
-    //       subtitle = "Yoda has spoken."; //add subtitle
-    //     } else {
-    //       quotePara.textContent = "Yoda doesn't seem to have anything to say right now. Click the Back button to try again."
-    //     }
-    // }
 
 var quote = function (chosenTag) {
   fetch("https://quotes15.p.rapidapi.com/quotes/random/", options)
@@ -253,7 +201,7 @@ var quote = function (chosenTag) {
       }
     })
     .catch(function (error) {
-      alert("Unable to connect to API");
+      alert("Unable to connect to Random Quote API");
     });
 };
 
